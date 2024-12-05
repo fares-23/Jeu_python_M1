8e5239f812c3c15df420b33618633a5e555f2d13
melaazbenoudiba <melaaz.benoudiba@etu.sorbonne-universite.fr>
2024-12-05 02:47:21 +0100

    mise a joiur assets

---
+++ assets/pipo-map001.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="pipo-map001" tilewidth="48" tileheight="48" tilecount="136" columns="8">
+ <image source="48x48/pipo-map001.png" width="384" height="816"/>
+</tileset>
---
+++ assets/pipo-map001_at - ÉRÉsÅ[.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="pipo-map001_at - ÉRÉsÅ[" tilewidth="48" tileheight="48" tilecount="480" columns="16">
+ <image source="48x48/pipo-map001_at - ÉRÉsÅ[.png" width="768" height="1440"/>
+</tileset>

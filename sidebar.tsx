46328a957dcb02039d371cded4e2a431830788ff
melaazbenoudiba <melaaz.benoudiba@etu.sorbonne-universite.fr>
2024-12-13 00:31:28 +0100

    réorganisation du projet

--- sidebar.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="sidebar" tilewidth="16" tileheight="16" tilecount="1802" columns="53">
- <image source="assets/interface/sidebar.png" width="862" height="551"/>
-</tileset>
